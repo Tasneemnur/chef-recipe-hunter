bc41fe794556091f7d633cef1d3942cf276b7dfb
Tasneem Nur <tasneemnurtashnia@gmail.com>
2023-05-04 13:33:54 +0600

    Footer added.

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>This is footer</h1>
-        </div>
-    );
+  return (
+    <div className="bg-dark">
+      <Container>
+        <footer className="legal-footer text-white mt-5 py-5">
+          <div className="row row-cols-2 row-cols-md-3">
+            <div className="border-end">
+              <h5 className="text-danger fw-bold mb-4">Subscribe</h5>
+              <p>
+              Subscribe for our latest & Articles. We Won't Give You Spam Mails
+              </p>
+              <form role="search">
+                <div className="d-flex">
+                  <input
+                    className="form-control me-2"
+                    type="email"
+                    placeholder="Your Email Address"
+                    aria-label="email"
+                  />
+                  <div>
+                    <Button className="bg-danger border-0" type="email">
+                      Subscribe
+                    </Button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="border-end px-5">
+              <h5 className="text-danger fw-bold mb-4">Explore</h5>
+              <ul className="list-group">
+                <Link to="/" className="text-white text-decoration-none">
+                  Home
+                </Link>
+                <Link to="/blog" className="text-white text-decoration-none">
+                  Blog
+                </Link>
+                <Link to="/login" className="text-white text-decoration-none">
+                  Login
+                </Link>
+              </ul>
+            </div>
+            <div className="px-5">
+              <h5 className="text-danger fw-bold mb-4">Contact</h5>
+              <p>787 Mark View Street, New Town, California</p>
+              <p>666 888 0000</p>
+              <p>info@gmail.com</p>
+            </div>
+          </div>
+        </footer>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/FoodCategory/FoodCategory.jsx
+++ src/components/FoodCategory/FoodCategory.jsx
@@ -11,13 +11,13 @@ const FoodCategory = () => {
     return (
         <div className='py-5'>
              <h2 className='text-center my-5 fw-bold'>Recipe <span className='text-danger'>Categories</span></h2>
-             <div className='row row-cols-2 row-cols-md-5 gap-0'>
+             <div className='row row-cols-2 row-cols-md-5'>
              <div>
-                <img src={appetizers} alt="" srcset="" style={{height:"180px", width:"180px"}} className='rounded-circle' />
+                <img src={appetizers} alt="" srcset="" style={{height:"170px", width:"170px"}} className='rounded-circle' />
                 <h4 className='text-center my-3 text-muted'>Appetizers</h4>
              </div>
              <div>
-                <img src={beef} alt="" srcset="" style={{height:"180px", width:"180px"}} className='rounded-circle' />
+                <img src={beef} alt="" srcset="" style={{height:"170px", width:"170px"}} className='rounded-circle' />
                 <h4 className='text-center my-3 text-muted'>Beef</h4>
              </div>
              <div>
@@ -25,11 +25,11 @@ const FoodCategory = () => {
                 <h4 className='text-center my-3 text-muted'>Chicken</h4>
              </div>
              <div>
-                <img src={Vegetarian} alt="" srcset="" style={{height:"180px", width:"180px"}} className='rounded-circle' />
+                <img src={Vegetarian} alt="" srcset="" style={{height:"170px", width:"170px"}} className='rounded-circle' />
                 <h4 className='text-center my-3 text-muted'>Vegetarian</h4>
              </div>
              <div>
-                <img src={dessert} alt="" srcset="" style={{height:"180px", width:"180px"}} className='rounded-circle' />
+                <img src={dessert} alt="" srcset="" style={{height:"170px", width:"170px"}} className='rounded-circle' />
                 <h4 className='text-center my-3 text-muted'>Desserts</h4>
              </div>
              </div>
